4db126a9a2f60492d606445af91babb869f69b39
yuvalgos <yuvalgos@gmail.com>
2024-01-28 17:19:27 +0200

    removed redundant lines

--- scratch/opencv_stream_camera.py
+++ scratch/opencv_stream_camera.py
@@ -6,13 +6,6 @@ import cv2
 pipeline = rs.pipeline()
 config = rs.config()
 
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
