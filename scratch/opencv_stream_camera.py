e0a37d0af41c70513ce22cfc4594fd7f1496a37f
yuvalgos <yuvalgos@gmail.com>
2024-01-28 17:28:56 +0200

    removed more redundant lines

--- scratch/opencv_stream_camera.py
+++ scratch/opencv_stream_camera.py
@@ -27,9 +27,6 @@ while True:
     # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-    depth_colormap_dim = depth_colormap.shape
-    color_colormap_dim = color_image.shape
-
     images = np.hstack((color_image, depth_colormap))
 
     # Show images
