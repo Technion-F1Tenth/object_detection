c884452cb7bb8dcced606f3f76bfdd2504f318b1
Toam-E <elharar.toam@campus.technion.ac.il>
2024-02-04 15:17:42 +0200

    Update opencv_stream_camera.py

--- object_detection/opencv_stream_camera.py
+++ object_detection/opencv_stream_camera.py
@@ -66,5 +66,8 @@ def main(buffer = 1):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    args_num = len(sys.argv[1])
+    if not args_num: main()
+    elif args_num == 1: main(sys.argv[1])
+    else: print("Too many arguments!")
 
