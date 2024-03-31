edd6765fe16dfddc99168ff67dda59b795386085
DoofWagonCar <doof.wagon.f1tenth@gmail.com>
2024-03-31 16:41:58 +0300

    logs

--- object_detection/object_detection/mainObjectDetection.py
+++ object_detection/object_detection/mainObjectDetection.py
@@ -38,13 +38,13 @@ class ObjectDetection:
         timestamp = time.localtime()
         formatted_time = time.strftime("%d-%m-%Y_%H-%M-%S", timestamp)
         log_name = "ObjectDetection_"+ formatted_time +".log"
-        log_file = logs_dir + log_name
-        printColor("the log file is: " + log_file, "\033[95m")
+        self.log_file = logs_dir + log_name
+
         
         logging.basicConfig(
             level=logging.DEBUG if self.debug_mode else logging.INFO,  # Set the logging level
             format='%(asctime)s - %(levelname)s - %(message)s',  # Set the log format
-            filename=log_file,  # Specify the log file
+            filename=self.log_file,  # Specify the log file
             filemode='w'  # Set the file mode ('w' for write, 'a' for append)
         )
         logging.info("!!!!!!!START!!!!!!!")
@@ -69,15 +69,12 @@ class ObjectDetection:
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
         # Define the video resolution based on the first image
-        clean_video = logs_dir + "ObjectDetection_" + formatted_time + "_clean.mp4"
-        data_video = logs_dir + "ObjectDetection_" + formatted_time + "_withData.mp4"
-        printColor(clean_video, "\033[93m")
-        printColor(data_video, "\033[93m")
+        self.clean_video_path = logs_dir + "ObjectDetection_" + formatted_time + "_clean.mp4"
+        self.data_video_path = logs_dir + "ObjectDetection_" + formatted_time + "_withData.mp4"
+               
         
-        
-        
-        self.clean_video = cv2.VideoWriter(clean_video, fourcc, 10, (640, 480))
-        self.data_video = cv2.VideoWriter(data_video, fourcc, 10, (640, 480))
+        self.clean_video = cv2.VideoWriter(self.clean_video_path, fourcc, 10, (640, 480))
+        self.data_video = cv2.VideoWriter(self.data_video_path, fourcc, 10, (640, 480))
 
     def getOpponentXYPoint(self, box, offset_x, offset_y):
         if box is not None:
@@ -108,52 +105,60 @@ class ObjectDetection:
         return np.mean(depth_list), c, r
         
     def runObjectDetection(self, buffer = 1, ros = False):
-        while True:
-            # Wait for a coherent pair of frames: depth and color
-            frames = self.pipeline.wait_for_frames()
-            depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
-            if not depth_frame or not color_frame:
-                continue
+    	try:    
+            while True:
+                # Wait for a coherent pair of frames: depth and color
+                frames = self.pipeline.wait_for_frames()
+                depth_frame = frames.get_depth_frame()
+                color_frame = frames.get_color_frame()
+                if not depth_frame or not color_frame:
+                    continue
 
-            # Convert images to numpy arrays
-            color_image = np.asanyarray(color_frame.get_data())
+                # Convert images to numpy arrays
+                color_image = np.asanyarray(color_frame.get_data())
 
-            bounding_box, output_images = self.detector.detect(color_image, return_images=True)
-            
-            if not ros:
-                cv2.namedWindow('detect', cv2.WINDOW_AUTOSIZE)
-                cv2.imshow('detect', output_images)
-                cv2.waitKey(1)
-
-            print("Write images to the clean video")
-            self.clean_video.write(color_image)
-
-
-            logging.debug(bounding_box)
-
-            if bounding_box is not None:
-                # c, r = self.get_opponent_xy_point(bounding_box)
-
-                # depth = depth_frame.get_distance(c, r)
-                # depth_point_in_meters_camera_coords = rs.rs2_deproject_pixel_to_point(self.depth_intrin, [c, r], depth)
-                depth_point_in_meters_camera_coords, c, r = self.getDepth(depth_frame, bounding_box)
-                logging.info('depth_point_in_meters_camera_coords is:' + str(depth_point_in_meters_camera_coords))
-
-                cv2.rectangle(output_images, (c-self.depth_radius, r-self.depth_radius), (c+self.depth_radius, r+self.depth_radius), (255, 0, 0), 2)  # Blue color bbox with 2px thickness
-                cv2.putText(output_images, "distance" + str(depth_point_in_meters_camera_coords),(20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2) 
-                print("Write images to the data video")
-                self.data_video.write(output_images)
+                bounding_box, output_images = self.detector.detect(color_image, return_images=True)
                 
-                if ros:
-                    return [c,r,depth_point_in_meters_camera_coords], output_images
-            else:
-                logging.info('boxes is None! no detections')
-                self.data_video.write(color_image)
-                if ros:
-                    return None, None
-        self.clean_video.release()
-        self.data_video.release()
+                if not ros:
+                    cv2.namedWindow('detect', cv2.WINDOW_AUTOSIZE)
+                    cv2.imshow('detect', output_images)
+                    cv2.waitKey(1)
+
+                print("Write images to the clean video")
+                self.clean_video.write(color_image)
+
+
+                logging.debug(bounding_box)
+
+                if bounding_box is not None:
+                    # c, r = self.get_opponent_xy_point(bounding_box)
+
+                    # depth = depth_frame.get_distance(c, r)
+                    # depth_point_in_meters_camera_coords = rs.rs2_deproject_pixel_to_point(self.depth_intrin, [c, r], depth)
+                    depth_point_in_meters_camera_coords, c, r = self.getDepth(depth_frame, bounding_box)
+                    logging.info('depth_point_in_meters_camera_coords is:' + str(depth_point_in_meters_camera_coords))
+
+                    cv2.rectangle(output_images, (c-self.depth_radius, r-self.depth_radius), (c+self.depth_radius, r+self.depth_radius), (255, 0, 0), 2)  # Blue color bbox with 2px thickness
+                    cv2.putText(output_images, "distance" + str(depth_point_in_meters_camera_coords),(20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2) 
+                    print("Write images to the data video")
+                    self.data_video.write(output_images)
+                    
+                    if ros:
+                        return [c,r,depth_point_in_meters_camera_coords], output_images
+                else:
+                    logging.info('boxes is None! no detections')
+                    self.data_video.write(color_image)
+                    if ros:
+                        return None, None
+            self.clean_video.release()
+            self.data_video.release()
+        except KeyboardInterrupt:
+            print("Keyboard interrupt detected")
+        finally:
+            printColor(self.log_file, "\033[94m")
+            printColor(self.clean_video_path, "\033[93m")
+            printColor(self.data_video_path, "\033[93m")
+            
 
 def printColor(text, color_code="\033[0m"):
     print(f"{color_code}{text}\033[0m")
